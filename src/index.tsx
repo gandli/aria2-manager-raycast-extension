da40140a1e26c17e4c954dce2b7e3a203b2d6bcb
gandli <gandli@qq.com>
2023-06-07 23:09:23 +0800

    ✏️ fix: 解决一个循环渲染的问题

--- src/index.tsx
+++ src/index.tsx
@@ -1,70 +1,43 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import TasksList from "./components/TasksList";
 import useAria2 from "./hooks/useAria2";
 import { Task, Filter } from "./types";
 
-type State = {
-  isLoading: boolean;
-  searchText: string;
-  filter: Filter;
-  tasks: Task[];
-  visibleTasks: Task[];
-};
-
 export default function Command() {
   const { fetchTasks, isConnected } = useAria2();
-  const [state, setState] = useState<State>({
-    filter: Filter.All,
-    isLoading: true,
-    searchText: "",
-    tasks: [],
-    visibleTasks: [],
-  });
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [filter, setFilter] = useState<Filter>(Filter.All);
 
   useEffect(() => {
     const fetchData = async () => {
       if (isConnected) {
         const tasks = await fetchTasks();
-        setState((prevState) => ({
-          ...prevState,
-          tasks,
-          isLoading: false,
-        }));
+        setTasks(tasks);
       }
     };
 
     fetchData();
   }, [fetchTasks, isConnected]);
 
-  useEffect(() => {
-    setState((prevState) => ({
-      ...prevState,
-      visibleTasks: filterTasks(prevState.filter, prevState.tasks),
-    }));
-  }, [state.filter, state.tasks]);
+  const filteredTasks = useMemo(() => filterTasks(filter), [filter, tasks]);
 
-  const filterTasks = (filter: Filter, tasks: Task[]): Task[] => {
-    // 根据当前状态筛选并更新可见任务列表
+  const handleFilterChange = (filter: Filter) => {
+    setFilter(filter);
+  };
+
+  function filterTasks(filter: Filter): Task[] {
     return tasks.filter((task) => {
       if (filter === Filter.Active) {
-        return task.status === "active"; // 筛选正在下载的任务
+        return task.status === "active";
       } else if (filter === Filter.Waiting) {
-        return task.status === "waiting"; // 筛选正在等待的任务
+        return task.status === "waiting";
       } else if (filter === Filter.CompletePaused) {
-        return ["complete", "paused"].includes(task.status); // 筛选已完成/已停止的任务
+        return ["complete", "paused"].includes(task.status);
       } else {
         return true;
       }
     });
-  };
+  }
 
-  const handleFilterChange = (filter: Filter) => {
-    setState((prevState) => ({
-      ...prevState,
-      filter,
-      visibleTasks: filterTasks(filter, prevState.tasks),
-    }));
-  };
-
-  return <TasksList tasks={state.visibleTasks} onFilterChange={handleFilterChange} />;
+  return <TasksList tasks={filteredTasks} onFilterChange={handleFilterChange} />;
 }
